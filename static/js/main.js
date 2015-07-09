62f210c7baaf02cc2b9e6b32626df07be4d6cbce
Anny <annyxni@gmail.com>
2015-07-09 00:43:29 -0400

    sizing issue

--- static/js/main.js
+++ static/js/main.js
@@ -52,67 +52,73 @@ app.controller('MemberCtrl', function (/* $scope, $location, $http */) {
   this.members = [
         {
             name: "Annie Chen",
-            position_year: "President | Information Systems '16",
+            position: "President",
+            major_year: "Information Systems '16",
             description: "I'm a SoCal girl who enjoys cooking, gaming, reading manga, cosplaying, and helping others. I love hanging out with my ARCC family! &#9829;"
         },
         {
             name: "Louise Zhou",
-            position_year: "Vice President | Business Administration '17",
+            position: "Vice President",
+            major_year: " Business Administration '17",
             description: "Louise is from NorCal and believes firmly in Pearl Milk Tea, not Boba, not Bubble Tea. Louise enjoys eating pumpkin pie, watching dramas/anime, reading manga, and listening to k-pop. She is married to EXO's Kai."       
         },
         {
             name: "Lucy Tai",
-            position_year: "Secretary | ",
-            description: "Merp derp."       
+            position: "Secretary",
+            major_year: "",
+            description: ""       
         },
         {
             name: "Michelle Ma",
-            position_year: "Treasurer | ",
+            position: "Treasurer",
+            major_year: "",
             description: ""       
         },
         {
             name: "Timothy Gao",
-            position_year: "ONiB Coordinator | ",
+            position: "ONiB Coordinator",
+            major_year: "",
             description: ""       
         },
         {
             name: "Mercy Soong",
-            position_year: "Performance Chair | ",
+            position: "Performance Chair",
             description: ""       
         },
         {
             name: "Rundong Jiang",
-            position_year: "Culture Chair | ",
+            position: "Culture Chair",
+            major_year: "",
             description: ""       
         },
         {
             name: "Alicia Wu",
-            position_year: "PR Chair | ",
+            position: "PR Chair",
+            major_year: "",
             description: ""      
         },
         {
             name: "Clara Kim",
-            position_year: "Design Chair |",
+            position: "Design Chair",
+            major_year: "",
             description: ""       
         },
         {
             name: "Crystal Hou",
-            position_year: "Design Chair | ",
+            position: "Design Chair",
+            major_year: "",
             description: ""       
         },
         {
             name: "Elizabeth Zeng",
-            position_year: "Historian | ",
+            position: "Historian",
+            major_year: "",
             description: ""       
         },
         {
             name: "Ivan Wang",
-            position_year: "Webmaster | ",
-            description: ""       
-        },
-        {
-            name: "Alicia Wu",
-            position_year: "PR Chair | ",
+            position: "Webmaster ",
+            major_year: "",
             description: ""       
         }
     ];
