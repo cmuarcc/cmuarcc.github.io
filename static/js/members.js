3a97b5866963d92e4f8979ba5c4afea834c7d5d2
Anny <annyxni@gmail.com>
2015-07-09 00:09:18 -0400

    testing

--- static/js/members.js
+++ static/js/members.js
@@ -1,55 +1,6 @@
-/**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
+angular.module('memberListModel', [])
 
-/**
- * Main AngularJS Web Application
- */
-var app = angular.module('cmuARCCWebsite', [
-  'ngRoute'
-]);
-
-/**
- * Configure the Routes
- */
-app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    // Home
-    .when("/", {templateUrl: "static/partials/home.html", controller: "PageCtrl"})
-    // Pages
-    .when("/members", {templateUrl: "static/partials/members.html", controller: "MemberCtrl"})
-    .when("/events", {templateUrl: "static/partials/events.html", controller: "PageCtrl"})
-    .when("/about", {templateUrl: "static/partials/about.html", controller: "PageCtrl"})
-
-    .otherwise("/", {templateUrl: "partials/home.html", controller: "PageCtrl"});
-}]);
-
-/**
- * Controls the Blog
- */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
-
-/**
- * Controls the Member Profile Population
- */
-
-app.controller('MemberCtrl', function(/* $scope, $location, $http */) {
-  console.log("Member Controlller reporting for duty.");
-
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  });
-
-  this.members = [
+    .value('members', [
         {
             name: "Annie Chen",
             position_year: "President | Information Systems '16",
@@ -118,23 +69,5 @@ app.controller('MemberCtrl', function(/* $scope, $location, $http */) {
             position_year: "PR Chair | ",
             description: ""       
         }
-    ];
-
-});
-
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
-});
+    ]);
+    
